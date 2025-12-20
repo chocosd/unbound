5a76f8edab676505608857b40bfa64d35d654425
chocosd <hello@stevendix.co.uk>
2025-12-20 03:19:01 +0000

    feat(core): add chat input component with styling and button enhancements

---
+++ src/app/shared/button/button-sizes.enum.ts
@@ -0,0 +1,15 @@
+export enum ButtonSizes {
+  XSmall = 'xs',
+  Small = 's',
+  Medium = 'm',
+  Large = 'l',
+  XLarge = 'xl',
+  XXLarge = '2xl',
+}
+
+export enum Themes {
+  Primary = 'primary',
+  Secondary = 'secondary',
+  Tertiary = 'tertiary',
+  Clear = 'clear',
+}
